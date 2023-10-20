aa376eb2d39904f25225f7c9af1bd0d2ddbb78d4
Rushikesh Bunge <68096217+rushikesh1799@users.noreply.github.com>
2023-10-20 19:11:13 +0530

    refactor: improved ui of login and signup page

--- src/features/auth/components/Login.jsx
+++ src/features/auth/components/Login.jsx
@@ -2,63 +2,129 @@ import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import "./auth.css";
 import useLoginHandler from "../Hooks/useLoginHandler";
+import Navbar from "../../../components/Navbar/Navbar";
+import Sidebar from "../../../components/Sidebar/Sidebar";
+
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import { useLocation } from "react-router";
+import { Link } from "react-router-dom";
 
 const Login = () => {
     const dispatch = useDispatch();
-    // console.log(useLoginHandler());
     const { loginHandler } = useLoginHandler();
-    // console.log("loginHandler", loginHandler);
     const [login, setLogin] = useState({
         email: "",
         password: "",
     });
+    const location = useLocation();
+    const [showPassword, setShowPassword] = useState(false);
 
     return (
-        <div>
-            <h2>Login form</h2>
-            <form
-                action="submit"
-                onSubmit={(e) => {
-                    e.preventDefault();
-                    dispatch(loginHandler(login));
-                }}
-                className="signup_form"
-                autoComplete="off"
-            >
-                <div className="form_input">
-                    <label>Email:</label>
-                    <input
-                        autoFocus
-                        type="text"
-                        name="fullName"
-                        value={login.email}
-                        onChange={(e) =>
-                            setLogin((prev) => ({
-                                ...prev,
-                                email: e.target.value,
-                            }))
-                        }
-                        required
-                    />
+        <div className="pagewrapper">
+            <Navbar />
+            <div className="middle-content">
+                <Sidebar />
+                <div className="auth-container flex-column-center">
+                    <h2 className="header2">Login form</h2>
+                    <form
+                        action="submit"
+                        onSubmit={(e) => {
+                            e.preventDefault();
+                            if (login.email === "" || login.password === "") {
+                                alert("Please fill all the required details!");
+                            }
+                            dispatch(loginHandler(login));
+                        }}
+                        className="form-auth"
+                        autoComplete="off"
+                    >
+                        <div className="form-input">
+                            <label className="input-label">Email: *</label>
+                            <input
+                                className="input-primary border-box"
+                                autoFocus
+                                type="text"
+                                name="fullName"
+                                value={login.email}
+                                onChange={(e) =>
+                                    setLogin((prev) => ({
+                                        ...prev,
+                                        email: e.target.value,
+                                    }))
+                                }
+                                required
+                            />
+                        </div>
+                        <div className="form-input password-input">
+                            <label className="input-label">Password: *</label>
+                            <input
+                                className="input-primary border-box"
+                                autoFocus
+                                type={showPassword ? "text" : "password"}
+                                name="fullName"
+                                value={login.password}
+                                onChange={(e) =>
+                                    setLogin((prev) => ({
+                                        ...prev,
+                                        password: e.target.value,
+                                    }))
+                                }
+                                required
+                            />
+                            <button
+                                className="btn-no-decoration cursor-pointer text-white"
+                                type="button"
+                                // disabled={disableLogin}
+                                onClick={() => setShowPassword(!showPassword)}
+                            >
+                                {showPassword ? (
+                                    <VisibilityIcon
+                                        sx={{
+                                            position: "absolute",
+                                            top: "4px",
+                                            right: "24px",
+                                        }}
+                                        className="input-icon-style"
+                                    />
+                                ) : (
+                                    <VisibilityOffIcon
+                                        sx={{
+                                            position: "absolute",
+                                            top: "4px",
+                                            right: "24px",
+                                        }}
+                                        className="input-icon-style"
+                                    />
+                                )}
+                            </button>
+                        </div>
+                        <button className="btn-auth">Login</button>
+                        <button
+                            className="btn-outline-primary btn-auth guest-button"
+                            onClick={(e) => {
+                                setLogin((prev) => ({
+                                    ...prev,
+                                    email: e.target.value,
+                                    password: e.target.value,
+                                }));
+                            }}
+                        >
+                            Login as Guest
+                        </button>
+                        <div className="flex-row-center account-line-margin">
+                            <span>Don't have an account?</span>
+                            <Link
+                                to="/signup"
+                                className="btn-link btn-link-primary1"
+                                state={location.state}
+                            >
+                                Create One
+                            </Link>
+                        </div> 
+                    </form>
                 </div>
-                <div className="form_input">
-                    <label>Password:</label>
-                    <input
-                        autoFocus
-                        type="text"
-                        name="fullName"
-                        value={login.password}
-                        onChange={(e) =>
-                            setLogin((prev) => ({
-                                ...prev,
-                                password: e.target.value,
-                            }))
-                        }
-                        required
-                    />
-                </div>
-                <button>Login</button>
-            </form>
+            </div>
         </div>
     );
 };
--- src/features/auth/components/Signup.jsx
+++ src/features/auth/components/Signup.jsx
@@ -1,10 +1,21 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import "./auth.css";
-import { signUpHandler } from "../authSlice";
+import Navbar from "../../../components/Navbar/Navbar";
+import Sidebar from "../../../components/Sidebar/Sidebar";
+
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import { useLocation } from "react-router";
+import { Link } from "react-router-dom";
+import useSignupHandler from "../Hooks/useSignupHandler";
 
 const Signup = () => {
     const dispatch = useDispatch();
+    const [showPassword, setShowPassword] = useState(false);
+    const location = useLocation();
+
+    const { signupHandler } = useSignupHandler();
 
     const [signup, setSignup] = useState({
         firstName: "",
@@ -14,83 +25,129 @@ const Signup = () => {
     });
 
     return (
-        <div>
-            <h2>Sign Up form</h2>
-            <form
-                action="submit"
-                onSubmit={(e) => {
-                    e.preventDefault();
-                    dispatch(signUpHandler(signup));
-                }}
-                className="signup_form"
-                autoComplete="off"
-            >
-                <div className="form_input">
-                    <label>First Name:</label>
-                    <input
-                        autoFocus
-                        type="text"
-                        name="fullName"
-                        value={signup.firstName}
-                        onChange={(e) =>
-                            setSignup((prev) => ({
-                                ...prev,
-                                firstName: e.target.value,
-                            }))
-                        }
-                        required
-                    />
+        <div className="pagewrapper">
+            <Navbar />
+            <div className="middle-content">
+                <Sidebar />
+                <div className="auth-container flex-column-center signup-container">
+                    <h2 className="header2">Sign Up form</h2>
+                    <form
+                        action="submit"
+                        onSubmit={(e) => {
+                            e.preventDefault();
+                            dispatch(signupHandler(signup));
+                        }}
+                        className="form-auth"
+                        autoComplete="off"
+                    >
+                        <div className="form-input">
+                            <label className="input-label">First Name: *</label>
+                            <input
+                                className="input-primary border-box"
+                                autoFocus
+                                type="text"
+                                name="fullName"
+                                value={signup.firstName}
+                                onChange={(e) =>
+                                    setSignup((prev) => ({
+                                        ...prev,
+                                        firstName: e.target.value,
+                                    }))
+                                }
+                                required
+                            />
+                        </div>
+                        <div className="form-input">
+                            <label className="input-label">Last Name: *</label>
+                            <input
+                                className="input-primary border-box"
+                                autoFocus
+                                type="text"
+                                name="fullName"
+                                value={signup.lastName}
+                                onChange={(e) =>
+                                    setSignup((prev) => ({
+                                        ...prev,
+                                        lastName: e.target.value,
+                                    }))
+                                }
+                                required
+                            />
+                        </div>
+                        <div className="form-input">
+                            <label className="input-label">Email: *</label>
+                            <input
+                                className="input-primary border-box"
+                                autoFocus
+                                type="text"
+                                name="fullName"
+                                value={signup.email}
+                                onChange={(e) =>
+                                    setSignup((prev) => ({
+                                        ...prev,
+                                        email: e.target.value,
+                                    }))
+                                }
+                                required
+                            />
+                        </div>
+                        <div className="form-input password-input">
+                            <label className="input-label">Password: *</label>
+                            <input
+                                className="input-primary border-box"
+                                autoFocus
+                                type={showPassword ? "text" : "password"}
+                                name="fullName"
+                                value={signup.password}
+                                onChange={(e) =>
+                                    setSignup((prev) => ({
+                                        ...prev,
+                                        password: e.target.value,
+                                    }))
+                                }
+                                required
+                            />
+                            <button
+                                className="btn-no-decoration cursor-pointer text-white"
+                                type="button"
+                                // disabled={disableLogin}
+                                onClick={() => setShowPassword(!showPassword)}
+                            >
+                                {showPassword ? (
+                                    <VisibilityIcon
+                                        sx={{
+                                            position: "absolute",
+                                            top: "4px",
+                                            right: "24px",
+                                        }}
+                                        className="input-icon-style"
+                                    />
+                                ) : (
+                                    <VisibilityOffIcon
+                                        sx={{
+                                            position: "absolute",
+                                            top: "4px",
+                                            right: "24px",
+                                        }}
+                                        className="input-icon-style"
+                                    />
+                                )}
+                            </button>
+                        </div>
+                        <button className="btn-auth">SIGN UP</button>
+                        <div className="flex-row-center account-line-margin">
+                            <span>Already have an account?</span>
+                            <Link
+                                to="/login"
+                                className="btn-link btn-link-primary1"
+                                state={location.state}
+                            >
+                                Login here
+                            </Link>
+                        </div>
+                    </form>
                 </div>
-                <div className="form_input">
-                    <label>Last Name:</label>
-                    <input
-                        autoFocus
-                        type="text"
-                        name="fullName"
-                        value={signup.lastName}
-                        onChange={(e) =>
-                            setSignup((prev) => ({
-                                ...prev,
-                                lastName: e.target.value,
-                            }))
-                        }
-                        required
-                    />
-                </div>
-                <div className="form_input">
-                    <label>Email:</label>
-                    <input
-                        autoFocus
-                        type="text"
-                        name="fullName"
-                        value={signup.email}
-                        onChange={(e) =>
-                            setSignup((prev) => ({
-                                ...prev,
-                                email: e.target.value,
-                            }))
-                        }
-                        required
-                    />
-                </div>
-                <div className="form_input">
-                    <label>Password:</label>
-                    <input
-                        autoFocus
-                        type="text"
-                        name="fullName"
-                        value={signup.password}
-                        onChange={(e) =>
-                            setSignup((prev) => ({
-                                ...prev,
-                                password: e.target.value,
-                            }))
-                        }
-                        required
-                    />
-                </div>
-                <button>Submit</button>
-            </form>
+            </div>
         </div>
     );
 };
